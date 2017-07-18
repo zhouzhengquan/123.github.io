32d8c96c147989220d4b0b3a353009dc46f72d56
zhouzhengquan <zzhou@merkleinc.com>
2017-07-18 11:23:27 +0800

    Add files via upload

--- ecommerce_dl_event.js
+++ ecommerce_dl_event.js
@@ -1,7 +1,7 @@
 var product_list= [{ 
-                'name': 'Triblend Android T-Shirt',  // Name or ID is required. 
+                'name': 'Test Product',  // Name or ID is required. 
                 'id': '12345', 
-                'price': '15.25', 
+                'price': '50', 
                 'brand': 'Google', 
                 'category': 'Apparel', 
                 'variant': 'Gray',
@@ -9,10 +9,10 @@ var product_list= [{
                 'position': 1
             }, 
             { 
-              'name': 'zzhou test product', 
-              'id': '00001', 
-              'price': '15000', 
-              'brand': 'zzhou', 
+              'name': 'Donut Friday Scented T-Shirt', 
+              'id': '67890', 
+              'price': '33.75', 
+              'brand': 'Google', 
               'category': 'Apparel',
                 'variant': 'Black', 
                 'list': 'Search Results',
@@ -30,506 +30,211 @@ var product_list= [{
               'position': 3                      
              }];
 
- function ecommerce_checkout_push(){
+var chars = ['0','1','2','3','4','5','6','7','8','9'];
+
+function generateMixed(n) {
+     var res = "";
+     for(var i = 0; i < n ; i ++) {
+         var id = Math.ceil(Math.random()*9);
+         res += chars[id];
+     }
+     return res;
+}
+
+var User_ID_generator = generateMixed(10)
+var Ecommerce_ID_generator = generateMixed(8)
+var Product_ID_generator = generateMixed(6)
+var Promo_ID = generateMixed(7)
+var Promo_Name = "zzhou" + Promo_ID
+
+function ecommerce_product_detail() {
+  dataLayer.push({
+    'event': 'GA-event',
+    'ecommerce': {
+      'detail': {
+      'products': [{                        //  adding a product to a shopping cart.
+             'name': 'Test Product',  // Name or ID is required. 
+             'id': '11111', 
+             'price': '66', 
+             'quantity': 16,
+             'brand': 'Google', 
+             'category': 'Apparel', 
+            'variant': 'Gray',
+            'list': 'Search Results', 
+       }]
+       }
+     }
+  });
+}
+
+ function ecommerce_add_to_cart(){
+   window.dataLayer.push({
+  'event': 'GA-event',
+  'ecommerce': {
+    'add': {                                // 'add' actionFieldObject measures.
+      'products': [{                        //  adding a product to a shopping cart.
+             'name': 'Test Product',  // Name or ID is required. 
+             'id': '11111', 
+             'price': '66', 
+             'quantity': 16,
+             'brand': 'Google', 
+             'category': 'Apparel', 
+            'variant': 'Gray',
+            'list': 'Search Results', 
+       }]
+    }
+  }
+});
+ }
+
+function promotion_click() {
+window.dataLayer.push({
+  'event': 'GA-event',
+  'ecommerce': {
+    'promoClick': {
+      'promotions': [                     // Array of promoFieldObjects.
+       {
+         'id': Promo_ID,            // ID or Name is required.
+         'name': Promo_Name,
+         'creative': 'banner1',
+         'position': 'slot1'
+       }],
+    }
+  }
+})
+}
+
+function promotion_view() {
+window.dataLayer.push({
+  'event': 'GA-event',
+  'ecommerce': {
+    'promoView': {
+      'promotions': [                     // Array of promoFieldObjects.
+       {
+         'id': Promo_ID,            // ID or Name is required.
+         'name': Promo_Name,
+         'creative': 'banner1',
+         'position': 'slot1'
+       }],
+    }
+  }
+})
+}
+
+ function ecommerce_checkout_push1(){
    window.dataLayer.push({
      'event': 'GA-event',
-     'page': {
-       'type': 'cart'
-     },
-     'user': {
-       'registered': 'false',
-       'status': 'logged-in',
-       'id': '555'
-     },
+
      'site': {
        'environment': 'live'
      },
      'ecommerce': {
        'checkout': {
          'actionField': {
-           'step': 1,
-           'option': 'Buy Now'
-         },
-         'products': [product_list[1]]
-       }
+           'step': 1,'option': 'Nanjing'},
+               'products': [{                            // List of productFieldObjects.
+                'name': 'Test Product',  // Name or ID is required. 
+                'id': '11111', 
+                'price': '66', 
+                'quantity': 16,
+                'brand': 'Google', 
+                'category': 'Apparel', 
+                'variant': 'Gray',
+                'list': 'Search Results', 
+       }]
+      }
      }
    });
  }
 
+
+ function ecommerce_checkout_push2(){
+   window.dataLayer.push({
+     'event': 'GA-test',
+     'site': {
+       'environment': 'live'
+     },
+     'ecommerce': {
+       'checkout': {
+         'actionField': {
+           'step': 2,
+           'option': 'Alipay'},
+               'products': [{                            // List of productFieldObjects.
+                'name': 'Test Product',  // Name or ID is required. 
+                'id': '11111', 
+                'price': '66', 
+                'quantity': 16,
+                'brand': 'Google', 
+                'category': 'Apparel', 
+                'variant': 'Gray',
+                'list': 'Search Results', 
+       }]
+      }
+     }
+   });
+ }
+
+ function ecommerce_checkout_push3(){
+   window.dataLayer.push({
+     'event': 'GA-event',
+     'site': {
+       'environment': 'live'
+     },
+     'ecommerce': {
+       'checkout': {
+         'actionField': {
+           'step': 3,
+           'option': 'Full Pay'},
+               'products': [{                            // List of productFieldObjects.
+                'name': 'Test Product',  // Name or ID is required. 
+                'id': '11111', 
+                'price': '66', 
+                'quantity': 16,
+                'brand': 'Google', 
+                'category': 'Apparel', 
+                'variant': 'Gray',
+                'list': 'Search Results', 
+       }]
+      }
+     }
+   });
+ }
+
+ 
+
+
+
 function confirm_purchase() {
   window.dataLayer.push({
     'event': 'GA-event',
-    'couponSavings': 0.00,
     'ecommerce': {
-      'currencyCode': 'USD',
+
       'purchase': {
         'actionField': {
-          'id': 'T67890',                         // Transaction ID. Required for purchases and refunds.
-          'affiliation': 'USA Store',
-          'revenue': '23000',
-          'tax':'12000'
+            'id': Ecommerce_ID_generator,   
         },
-         'products': [product_list[1]]
-      }
-    }
-  });
-}
-function product_click(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'click': {
-        'actionField': {'list': 'Search Results'},      // Optional list property.
-         'products': [product_list[1]]
-       }
-
-     }
-  });
-}
-
-function product_detail(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'detail': {
-        'actionField': {'list': 'Search Results'},      // Optional list property.
-         'products': [product_list[1]]
-       }
-     }
-  });
-}
-
-function product_add_to_cart(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'currencyCode': 'EUR',
-      'add': {                                // 'add' actionFieldObject measures.
-         'products': [product_list[1]]
-    }
-    }
-  });
-}
-
-function product_remove_to_cart(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'remove': {                               // 'remove' actionFieldObject measures.
-         'products': [product_list[1]]
-      }
-    }
-  });
-}
-
-//Measuring Promotion Impressions
-function measuring_promotion_pressions(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'promoView': {
-        'promotions': [                     // Array of promoFieldObjects.
-         {
-           'id': productObj[0].id,            // ID or Name is required.
-           'name': productObj[0].name,
-           'creative': productObj[0].creative,
-           'position': productObj[0].position,
-         },
-         {
-           'id': productObj[1].id,
-           'name': productObj[1].name,
-           'creative': productObj[1].creative,
-           'position': productObj[1].position
-         }]
-      }
-    }
-  });
-}
-
-//Measuring Promotion Clicks
-function onPromoClick(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'promoClick': {
-        'promotions': [
-         {
-           'id': productObj.id,                         // Name or ID is required.
-           'name': productObj.name,
-           'creative': productObj.creative,
-           'position': productObj.position
-         }]
-      }
-    }
-  });
-}
-
-//Measuring a Checkout
-function onCheckout(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'checkout': {
-        'actionField': {'step': 1, 'option': 'Visa'},
-        'products': [{
-          'name': productObj.name,
-          'id': productObj.id,
-          'price': productObj.price,
-          'brand': productObj.brand,
-          'category': productObj.category,
-          'variant': productObj.variant,
-          'quantity': productObj.quantity
+               'products': [{                            
+                'name': 'Test Product',  // Name or ID is required. 
+                'id': '11111', 
+                'price': '66', 
+                'quantity': 16,
+                    // Optional fields may be omitted or set to empty string.
        }]
-     }
-   }
-  });
-}
-
-//Measuring Checkout Options
-function onCheckoutOption(step, checkoutOption) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'checkout_option': {
-        'actionField': {'step': step, 'option': checkoutOption}
-      }
-    }
-  });
-}
-
-//Measuring Purchases
-function measuring_purchases(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'purchase': {
-        'actionField': {
-          'id': productObj[0].id,                         // Transaction ID. Required for purchases and refunds.
-          'affiliation': productObj[0].affiliation,
-          'revenue': productObj[0].revenue,                    // Total transaction value (incl. tax and shipping)
-          'tax': productObj[0].tax,
-          'shipping': productObj[0].shipping,
-          'coupon': productObj[0].coupon
-        },
-        'products': [{                            // List of productFieldObjects.
-          'name': productObj[1].name,    // Name or ID is required.
-          'id': productObj[1].id, 
-          'price': productObj[1].price, 
-          'brand': productObj[1].brand,
-          'category': productObj[1].category, 
-          'variant': productObj[1].variant,
-          'quantity': productObj[1].quantity,
-          'coupon': productObj[1].coupon                           // Optional fields may be omitted or set to empty string.
-         },
-         {
-          'name': productObj[2].name, 
-          'id': productObj[2].id,
-          'price': productObj[2].price,
-          'brand': productObj[2].brand,
-          'category': productObj[2].category,
-          'variant': productObj[2].variant,
-          'quantity': productObj[2].quantity
-         }]
-      }
-    }
-  });
-}
-
-//Measuring Refunds
-function measuring_refunds(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'refund': {
-        'actionField': {'id': 'T12345'},        // Transaction ID.
-        'products': [
-              {'id': productObj[0].id, 'quantity': productObj[0].quantity},   // Product ID and quantity. Required for partial refunds.
-              {'id': productObj[1].id,'quantity': productObj[1].quantity}
-         ]
-       }
-    }
-  });
-}
-
-//Passing Product-scoped Custom Dimensions
-function pass_custom_dimensions(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'purchase': {
-        'products': [{
-          'name': productObj.name,
-          'id': productObj.id,
-          'price': productObj.price,
-          'brand': productObj.brand,
-          'category': productObj.category,
-          'variant': productObj.variant,
-          'dimension1': productObj.dimension1
-         }]
-       }
-     }
-  });
-}
-
-//Passing Product-scoped Custom Metrics
-function pass_custom_metrics(productObj){
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'purchase': {
-        'products': [{
-          'name': productObj.name,
-          'id': productObj.id,
-          'brand': productObj.brand,
-          'category': productObj.category,
-          'variant': productObj.variant,
-          'price': productObj.price,
-          'quantity': productObj.quantity,
-          'metric3': productObj.metric3  // Custom metric 'Cost'
-         }]
-       }
-     }
-  });
-}
-
-//Combining Impressions and Actions
-function combining_impressions(productObj) {
-  dataLayer.push({
-    'event': 'GA-event',
-    'ecommerce': {
-      'impressions': [
+      },
+ 'promoView': {
+      'promotions': [                     // Array of promoFieldObjects.
        {
-         'name': productObj[0].name,        // Name or ID is required.
-         'id': productObj[0].id,
-         'price': productObj[0].price,
-         'brand': productObj[0].brand,
-         'category': productObj[0].category,
-         'variant': productObj[0].variant,
-         'list': productObj[0].list,
-         'position': productObj[0].position
-       },
-       {
-         'name': productObj[1].name,
-         'id': productObj[1].id,
-         'price': productObj[1].price,
-         'brand': productObj[1].brand,
-         'category': productObj[1].category,
-         'variant': productObj[1].variant,
-         'list': productObj[1].list,
-         'position': productObj[1].position
+         'id': Promo_ID,            // ID or Name is required.
+         'name': Promo_Name,
+         'creative': 'banner1',
+         'position': 'slot1'
        }],
-      'detail': {
-        'actionField': {'list': productObj[2].list},  // 'detail' actions have an optional list property.
-        'products': [{
-          'name': productObj[2].name,       // Name or ID is required.
-          'id': productObj[2].id,
-          'price': productObj[2].price,
-          'brand': productObj[2].brand,
-          'category': productObj[2].category,
-          'variant': productObj[2].variant
-         }]
-       }
     }
+
+    }
+    
   });
 }
 
-//Using a Custom JavaScript Macro
-function custom_javaScript_macro(productObj) {
-  var ecommerceData = {
-    'ecommerce': {
-      'purchase': {
-        'actionField': {'id': 'T12345'},
-        'products': [
-          {
-          'name': productObj.name,
-          'id': productObj.id,
-          'brand': productObj.brand,
-          'category': productObj.category,
-          'variant': productObj.variant,
-          'price': productObj.price,
-          'quantity': productObj.quantity,
-          'metric3': productObj.metric3  // Custom metric 'Cost'
-         }
-        ]
-      }
-    }
-  };
-  return ecommerceData;
-}
-var ga_wse_tracking = [];
 
-ga_wse_tracking.product_view = function() {
-  window.dataLayer.push({
-     'event': 'GA-event',
-     "eventDetail": { 
-        'category': 'ecommerce', 
-        'action': 'impressions',
-        'label': 'undefined'
-      }
-   });
-};
 
-ga_wse_tracking.view_product_details = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'ecommerce', 
-      'action': 'product detail',
-      'label': '[hybrid, premium]'
-    }
- });
-}
-
-ga_wse_tracking.user_dashboard = function () {
-    window.dataLayer.push({
-     'event': 'GA-event',
-     "eventDetail": { 
-        'category': 'ecommerce', 
-        'action': 'view order',
-        'label': 'undefined'
-      }
-   });
-}
-
-ga_wse_tracking.assessment = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'assessment', 
-      'action': 'start',
-      'label': 'undefined'
-    }
- });
-}
-
-ga_wse_tracking.assessment_dialog_close = function() {
-    window.dataLayer.push({
-     'event': 'GA-event',
-     "eventDetail": { 
-        'category': 'assessment', 
-        'action': 'dialog close',
-        'label': 'undefined'
-      }
-   });
-}
-
-ga_wse_tracking.free_trail = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'course', 
-      'action': 'trial',
-      'label': 'undefined'
-    }
- });
-}
-
-ga_wse_tracking.member_login = function() {
-    window.dataLayer.push({
-     'event': 'GA-event',
-     "eventDetail": { 
-        'category': 'account', 
-        'action': 'login',
-        'label': 'member'
-      }
-   });
-}
-
-ga_wse_tracking.non_member_login = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'account', 
-      'action': 'login',
-      'label': 'non member'
-    }
- });
-}
-
-ga_wse_tracking.view_user_account_detail = function() {
-    window.dataLayer.push({
-     'event': 'GA-event',
-     "eventDetail": { 
-        'category': 'account', 
-        'action': 'profile detail',
-        'label': 'undefined'
-      }
-   });
-}
-
-ga_wse_tracking.learning_goal_career_growth = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'lead', 
-      'action': 'profile detail',
-      'label': 'career growth'
-    }
- });
-}
-
-ga_wse_tracking.learning_goal_travel = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'lead', 
-      'action': 'learning goal',
-      'label': 'travel'
-    }
- });
-}
-
-ga_wse_tracking.learning_goal_personal_growth = function() {
-    window.dataLayer.push({
-     'event': 'GA-event',
-     "eventDetail": { 
-        'category': 'lead', 
-        'action': 'learning goal',
-        'label': 'personal growth'
-      }
-   });
-}
-
-ga_wse_tracking.learning_goal_career_exclusive_service = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'lead', 
-      'action': 'learning goal',
-      'label': 'exclusive service'
-    }
- });
-}
-
-ga_wse_tracking.learning_goal_for_my_teen = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'lead', 
-      'action': 'learning goal',
-      'label': 'for my teen'
-    }
- });
-}
-
-ga_wse_tracking.learning_goal_personal_detail_cta = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'lead', 
-      'action': 'learning goal',
-      'label': 'cta'
-    }
- });
-}
-
-ga_wse_tracking.find_nearest_location = function() {
-  window.dataLayer.push({
-   'event': 'GA-event',
-   "eventDetail": { 
-      'category': 'search', 
-      'action': 'find center',
-      'label': 'start'
-    }
- });
-}
